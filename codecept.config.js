31a577b27c24c0e6d18ee83ffa5d0466103451ba
Hernan Navarro <navarro.hernan97@gmail.com>
2023-02-20 12:48:54 -0300

    Indentation and report

--- codecept.config.js
+++ codecept.config.js
@@ -1,36 +1,43 @@
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
-    tests: './*_test.js',
-    output: './output',
-    helpers: {
-      Playwright: {
-        url: 'https://frontend-final-production.up.railway.app',
-        onRequest: () => {
-  
-        },
-        show: false,
-        browser: 'chromium'
-      }
-    },
-    include: {
-      I: './steps_file.js'
-    },
-    mocha:  {
-      "reporterOptions": {
-        "codeceptjs-cli-reporter": {
-          "stdout": "-",
-          "options": {
-            "steps": true,
-          }
-        },
-        "mocha-junit-reporter": {
-          "stdout": "./output/console.log",
-          "options": {
-            "mochaFile": "./output/result.xml"
-          },
-          "attachments": true //add screenshot for a failed test
-            }
-          }
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'https://frontend-final-production.up.railway.app',
+      onRequest: () => {
+
       },
-    name: 'integracion-final'
-  }
+      show: false,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  mocha: {
+    "reporterOptions": {
+      "codeceptjs-cli-reporter": {
+        "stdout": "-",
+        "options": {
+          "steps": true,
+        }
+      },
+      "mocha-junit-reporter": {
+        "stdout": "./output/console.log",
+        "options": {
+          "mochaFile": "./output/result.xml"
+        },
+        "attachments": true //add screenshot for a failed test
+      }
+    }
+  },
+  plugins: {
+    testomatio: {
+      enabled: true,
+      require: '@testomatio/reporter/lib/adapter/codecept',
+      apiKey: process.env.TESTOMATIO,
+    }
+  },
+  name: 'integracion-final'
+}
--- integration_test.js
+++ integration_test.js
@@ -1,15 +1,15 @@
 Feature('IntegrationTest');
 
 const expect = require('chai').expect;
-const {I} = inject();
+const { I } = inject();
 
-Scenario('Checking if Backend is already up', async () =>{
+Scenario('Checking if Backend is already up', async () => {
     I.amOnPage('https://backend-final-production.up.railway.app/value');
 });
 
 Scenario('Checking if Frontend is already up and buttons Render correctly', async () => {
     I.amOnPage('https://frontend-final-production.up.railway.app/');
-    I.see('ADD', {css: '#btn'});
-    I.see('SUBTRACT', {css: '#btn'});
-    I.see('RESTART', {css: '#btn'});
+    I.see('ADD', { css: '#btn-add' });
+    I.see('SUBTRACT', { css: '#btn-subtract' });
+    I.see('RESTART', { css: '#btn-restart' });
 });
